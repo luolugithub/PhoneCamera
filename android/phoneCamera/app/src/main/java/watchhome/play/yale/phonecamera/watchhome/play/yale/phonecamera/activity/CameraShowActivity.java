c6e3847800257622c4561739acbc1d0047d4e8ed
yuyunlong <yuyunlong@jovision.com>
2016-01-29 11:09:12 +0800

    硬编码 onpreviewframe

--- android/phoneCamera/app/src/main/java/watchhome/play/yale/phonecamera/watchhome/play/yale/phonecamera/activity/CameraShowActivity.java
+++ android/phoneCamera/app/src/main/java/watchhome/play/yale/phonecamera/watchhome/play/yale/phonecamera/activity/CameraShowActivity.java
@@ -2,17 +2,21 @@ package watchhome.play.yale.phonecamera.watchhome.play.yale.phonecamera.activity
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import java.io.IOException;
 
 import watchhome.play.yale.phonecamera.R;
+import watchhome.play.yale.phonecamera.watchhome.play.yale.phonecamera.codec.AvcEncoder;
 import watchhome.play.yale.phonecamera.watchhome.play.yale.phonecamera.util.LogUtils;
 
 public class CameraShowActivity extends AppCompatActivity implements SurfaceHolder.Callback, Camera.PreviewCallback, View.OnClickListener{
@@ -23,6 +27,13 @@ public class CameraShowActivity extends AppCompatActivity implements SurfaceHold
     private SurfaceHolder surfaceholder;
     private Camera camera = null;
     private Button switchButton;
+    AvcEncoder avcCodec;
+    int width = 720;
+    int height = 1280;
+    int framerate = 20;
+    int bitrate = 2500000;
+
+    byte[] h264 = new byte[width*height*3/2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,14 @@ public class CameraShowActivity extends AppCompatActivity implements SurfaceHold
 
         switchButton = (Button)findViewById(R.id.switch_camera);
         switchButton.setOnClickListener(this);
+
+        // 方法1 Android获得屏幕的宽和高
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        int screenWidth = screenWidth = display.getWidth();
+        int screenHeight = screenHeight = display.getHeight();
+
+        avcCodec = new AvcEncoder(screenWidth,screenHeight,framerate,bitrate);
     }
 
     @Override
@@ -51,6 +70,9 @@ public class CameraShowActivity extends AppCompatActivity implements SurfaceHold
             //设置预览监听
             camera.setPreviewDisplay(holder);
             Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewSize(width, height);
+            parameters.setPictureSize(width, height);
+            parameters.setPreviewFormat(ImageFormat.YV12);
 
             if (this.getResources().getConfiguration().orientation
                     != Configuration.ORIENTATION_LANDSCAPE) {
@@ -81,14 +103,21 @@ public class CameraShowActivity extends AppCompatActivity implements SurfaceHold
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         if (camera != null) {
+            camera.setPreviewCallback(null) ;
             camera.stopPreview();
             camera.release();
         }
+        avcCodec.close();
     }
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
-        LogUtils.i("recive msg ,data: "+data);
+        //编码视频数据
+        LogUtils.i("video encode", "data length : "+data.length);
+        if(avcCodec != null){
+            int ret = avcCodec.offerEncoder(data, h264);
+            LogUtils.d("video encode", "Video data encode ret: "+ret);
+        }
     }
 
     @Override
